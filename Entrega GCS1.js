ae6a5a68cfe6574d1ae81ec32f098c17dacd8c67
Victor Hugo Garcia Mendez <vicxstroll20ka@gmail.com>
2022-05-05 14:05:51 -0700

    first commit

---
+++ Entrega GCS1.js
@@ -0,0 +1,8 @@
+describe("trabajo final de Gestion de calidad de software", function(){
+
+    it("prueba 1", function(){
+        cy.visit("https://store.steampowered.com/?l=spanish")
+
+        cy.visit("https://www.storexppen.mx/")
+    })
+})
